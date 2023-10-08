2747cfd98dbe77f3abcca63767659c16c7480292
ksmnvsg <ksmnvsagi@gmail.com>
2023-10-07 23:26:48 -0600

    OpenGL initialized

---
+++ config.hpp
@@ -0,0 +1,4 @@
+#define GL_SILENCE_DEPRECATION
+
+#include <iostream>
+#include <GLFW/glfw3.h>
--- main.cpp
+++ main.cpp
@@ -1,5 +1,35 @@
-#include <iostream>
+#include "config.hpp"
 
-int main() {
-    std::cout << "Hello, from 3d_engine!\n";
+int main(void) {
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.3, 0.3, 0.3, 1);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
